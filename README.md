5da689ae9571574b40ed94a117931afdb7b08795
chao <dc10101@qq.com>
2018-03-31 22:25:32 +0800

    test complete

--- README.md
+++ README.md
@@ -1,17 +1,44 @@
 # restrict-ip-koa-middleware
 
+## 用途
+
+这个中间件用来限制来访者的 IP 地址，起到类似防火墙的作用。
+
+## 功能
+
+- 白名单通过策略：只有白名单内的 IP 允许访问
+- 黑名单拦截策略：只有黑名单内的 IP 不允许访问
+- 内网地址通过：与白名单策略配合使用，允许不在白名单内的 IP 地址通过
+- 自定义方式获取 IP 地址：取 IP 地址可用自定义方式，例如从 Header 里取 x-forwarded-for 或者 x-real-ip 字段等
+- 自定义拦截后的处理方法：可以拦截、允许通过、自定义返回消息体
+
+## 默认行为
+-
+
 ## TDD
 
-- [*] 白名单策略 通过
-- [] 白名单策略 拦截
-- [] 白名单内网地址策略 通过
-- [] 白名单内网地址策略 拦截
+```
+  白名单外网地址
+    ✓ 在白名单，通过
+    ✓ 不在白名单，拦截
 
-- [] 黑名单策略 通过
-- [] 黑名单策略 拦截
+  白名单且允许内网地址
+    ✓ 不在白名单，但是本机地址 通过
+    ✓ 不在白名单，但是是 A 类内网地址 通过
+    ✓ 不在白名单，但是是 B 类内网地址 通过
+    ✓ 不在白名单，但是是 C 类内网地址 通过
+    ✓ 不在白名单，也不是内网地址 拦截
 
-- [] 自定义拦截函数 onRestrict
+  黑名单策略
+    ✓ 不在黑名单 通过
+    ✓ 在黑名单 拦截
 
-- [] trustedHeaderSequence 不指定，默认先 x-forwarded-for 后 x-real-ip
-- [] trustedHeaderSequence 自定义，按照顺序取 IP
-- [] trustedHeaderSequence = []，看直接 IP
+  自定义函数拦截
+    ✓ 自定义拦截函数 通过
+    ✓ 自定义拦截函数 拦截
+
+  trustedHeaderSequence
+    ✓ trustedHeaderSequence 不指定，默认先 x-forwarded-for 后 x-real-ip
+    ✓ trustedHeaderSequence 按指定顺序
+    ✓ trustedHeaderSequence 为空数组，看直接 IP
+```
--- test/test.js
+++ test/test.js
@@ -1,31 +1,30 @@
+const restrictIp = require('../index')
 const assert = require('assert')
 const request = require('supertest')
 const Koa = require('koa')
 const Router = require('koa-router')
-const bodyParser = require('koa-bodyparser')
-const router = new Router()
-const restrictIp = require('../index')
 
 const app = new Koa()
 
+app.use(async (ctx, next) => {
+  try{
+    await next()
+  }
+  catch(e){
+    if(e.message === 'IP restricted'){
+      ctx.status = 403
+      ctx.body = e.message + ': ' + e.ip
+    }
+  }
+})
 
-// app.use(async (ctx, next) => {
-//   try{
-//     await next()
-//   }
-//   catch(e){
-//     console.error(e)
-//     ctx.body = e
-//   }
-// })
-
-// app.use(bodyParser())
+const router = new Router()
 app.use(router.routes())
 
 const server = app.listen()
 
+let whitelist = ['2.2.2.2', '3.3.3.3']
 let blacklist = ['4.4.4.4']
-let whitelist = ['5.5.5.5', '6.6.6.6']
 
 const whitelistRestrict = restrictIp({
   whitelist: new Set(whitelist)
@@ -40,37 +39,154 @@ const blacklistRestrict = restrictIp({
   blacklist: new Set(blacklist)
 })
 
+const customHandlerRestrict = restrictIp({
+  blacklist: new Set(blacklist),
+  onRestrict: async (ctx, next) => {
+    if(ctx.query.hasPassport){
+      await next()
+    }
+    else{
+      ctx.status = 403
+    }
+  }
+})
+
+const defaultTrustedHeaderSequenceRestrict = restrictIp({
+  blacklist: new Set(blacklist)
+})
+
+const customTrustedHeaderSequenceRestrict = restrictIp({
+  blacklist: new Set(blacklist),
+  trustedHeaderSequence: ['x-real-ip', 'x-forwarded-for']
+})
+
+const noTrustedHeaderSequenceRestrict = restrictIp({
+  blacklist: new Set(blacklist),
+  trustedHeaderSequence: []
+})
+
 async function passed(ctx, next) {
   ctx.body = 'passed'
 }
 
-let ctrl = async ctx => {
-  ctx.body = 'passed'
-  // ctx.body = {a:1}
-}
+router.get('/', whitelistRestrict, passed)
+router.get('/1', whitelistAllowPrivateRestrict, passed)
+router.get('/2', blacklistRestrict, passed)
+router.get('/3', customHandlerRestrict, passed)
+router.get('/4', defaultTrustedHeaderSequenceRestrict, passed)
+router.get('/5', customTrustedHeaderSequenceRestrict, passed)
+router.get('/6', noTrustedHeaderSequenceRestrict, passed)
 
-router.get('/', ctrl)
-
-// router.get('/', whitelistRestrict, passed)
-// router.get('/header/none', whitelistRestrict, passed)
-// router.get('/header/x-forwarded-for', whitelistRestrict, passed)
-// router.get('/header/x-real-ip', whitelistRestrict, passed)
-
-
-const publicIp = '5.5.5.5'
-const privateIp = '10.0.0.1'
-const xForwardedFor = `${publicIp}, ${privateIp}`
-const xRealIp = '4.4.4.4'
-
-describe('Public IP', function () {
-  it('1. 白名单策略 通过', async function () {
+describe('白名单外网地址', function () {
+  it('在白名单，通过', async function () {
     let fakeIp = '2.2.2.2'
-    let resp = await request(server)
+    await request(server)
       .get('/')
       .set('x-real-ip', fakeIp)
       .expect(200)
-
-    console.log(JSON.stringify(resp))
+  })
+  it('不在白名单，拦截', async function () {
+    let fakeIp = '9.9.9.9'
+    await request(server)
+      .get('/')
+      .set('x-real-ip', fakeIp)
+      .expect(403)
   })
 })
 
+describe('白名单且允许内网地址', function () {
+  it('不在白名单，但是本机地址 通过', async function () {
+    await request(server)
+      .get('/1')
+      .expect(200)
+  })
+  it('不在白名单，但是是 A 类内网地址 通过', async function () {
+    let fakeIp = '10.0.0.2'
+    await request(server)
+      .get('/1')
+      .set('x-real-ip', fakeIp)
+      .expect(200)
+  })
+  it('不在白名单，但是是 B 类内网地址 通过', async function () {
+    let fakeIp = '172.16.0.2'
+    await request(server)
+      .get('/1')
+      .set('x-real-ip', fakeIp)
+      .expect(200)
+  })
+  it('不在白名单，但是是 C 类内网地址 通过', async function () {
+    let fakeIp = '192.168.0.2'
+    await request(server)
+      .get('/1')
+      .set('x-real-ip', fakeIp)
+      .expect(200)
+  })
+  it('不在白名单，也不是内网地址 拦截', async function () {
+    let fakeIp = '9.9.9.9'
+    await request(server)
+      .get('/1')
+      .set('x-real-ip', fakeIp)
+      .expect(403)
+  })
+})
+
+describe('黑名单策略', function () {
+  it('不在黑名单 通过', async function () {
+    await request(server)
+      .get('/2')
+      .expect(200)
+  })
+  it('在黑名单 拦截', async function () {
+    let fakeIp = '4.4.4.4'
+    await request(server)
+      .get('/2')
+      .set('x-real-ip', fakeIp)
+      .expect(403)
+  })
+})
+
+describe('自定义函数拦截', function () {
+  it('自定义拦截函数 通过', async function () {
+    let fakeIp = '4.4.4.4'
+    await request(server)
+      .get('/3?hasPassport=1')
+      .set('x-real-ip', fakeIp)
+      .expect(200)
+  })
+  it('自定义拦截函数 拦截', async function () {
+    let fakeIp = '4.4.4.4'
+    let a = await request(server)
+      .get('/3')
+      .set('x-real-ip', fakeIp)
+      .expect(403)
+  })
+})
+
+describe('trustedHeaderSequence', function () {
+  it('trustedHeaderSequence 不指定，默认先 x-forwarded-for 后 x-real-ip', async function () {
+    let fakeIp = '4.4.4.4'
+    await request(server)
+      .get('/4')
+      .set('x-forwarded-for', fakeIp)
+      .set('x-real-ip', '127.0.0.1')
+      .expect(403)
+  })
+
+  it('trustedHeaderSequence 按指定顺序', async function () {
+    let fakeIp = '4.4.4.4'
+    await request(server)
+      .get('/5')
+      .set('x-forwarded-for', fakeIp)
+      .set('x-real-ip', '127.0.0.1')
+      .expect(200)
+  })
+
+  it('trustedHeaderSequence 为空数组，看直接 IP', async function () {
+    let fakeIp = '4.4.4.4'
+    await request(server)
+      .get('/6')
+      .set('x-forwarded-for', fakeIp)
+      .set('x-real-ip', fakeIp)
+      .expect(200)
+  })
+})
